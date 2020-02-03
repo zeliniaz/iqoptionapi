98d8214ca68ec406f21612777d40ca700256ce18
Evecimar <silva.evecimar@gmail.com>
2020-02-03 08:17:55 -0300

    Send balance_id whith digital order

--- iqoptionapi/ws/chanels/digital_option.py
+++ iqoptionapi/ws/chanels/digital_option.py
@@ -8,15 +8,17 @@ from iqoptionapi.ws.chanels.base import Base
 
 class Digital_options_place_digital_option(Base):
     name = "sendMessage"
-    def __call__(self,instrument_id,amount):
+    def __call__(self,instrument_id,amount, user_balance_id=None):
+        if user_balance_id==None:
+            user_balance_id = int(self.api.profile.balance_id)
+        
         data = {
         "name": "digital-options.place-digital-option",
         "version":"1.0",
         "body":{
-            "user_balance_id":int(self.api.profile.balance_id),
+            "user_balance_id": user_balance_id,
             "instrument_id":str(instrument_id),
             "amount":str(amount)
-            
             }
         }
         self.send_websocket_request(self.name, data)
