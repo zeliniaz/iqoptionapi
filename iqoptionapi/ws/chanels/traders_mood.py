b7e3325e1da3102a3d73f6253e90fc5a4ecabafe
Rafael Faria <rfaaelr.faria@yahoo.com.br>
2020-03-18 03:22:54 -0300

    add support to forex on trade mood

--- iqoptionapi/ws/chanels/traders_mood.py
+++ iqoptionapi/ws/chanels/traders_mood.py
@@ -4,46 +4,40 @@ from iqoptionapi.ws.chanels.base import Base
 
 
 class Traders_mood_subscribe(Base):
-   
     name = "subscribeMessage"
 
-    def __call__(self,active):
-       
-         
+    def __call__(self, active, instrument="turbo-option"):
         data = {
             "name": "traders-mood-changed",
             "params":
-                    {
+                {
                     "routingFilters":
-                                    {
-                                        "instrument":"turbo-option",
-                                        "asset_id":active
-                                    }
-                    }
+                        {
+                            "instrument": instrument,
+                            "asset_id": active
+                        }
+                }
 
         }
 
         self.send_websocket_request(self.name, data)
+
 
 class Traders_mood_unsubscribe(Base):
-   
     name = "unsubscribeMessage"
 
-    def __call__(self,active):
-       
-         
+    def __call__(self, active, instrument="turbo-option"):
         data = {
             "name": "traders-mood-changed",
             "params":
-                    {
+                {
                     "routingFilters":
-                                    {
-                                        "instrument":"turbo-option",
-                                        "asset_id":active
-                                    }
-                    }
+                        {
+                            "instrument": instrument,
+                            "asset_id": active
+                        }
+                }
 
         }
 
         self.send_websocket_request(self.name, data)
-
