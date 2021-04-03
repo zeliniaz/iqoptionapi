7dce9067aa58227de23322fdb7acdde28c6bc924
Rafael Faria <rafaelr.faria@yahoo.com.br>
2021-04-03 18:42:37 -0300

    update version

--- iqoptionapi/stable_api.py
+++ iqoptionapi/stable_api.py
@@ -22,7 +22,7 @@ def nested_dict(n, type):
 
 
 class IQ_Option:
-    __version__ = "7.0"
+    __version__ = "7.0.0"
 
     def __init__(self, email, password, active_account_type="PRACTICE"):
         self.size = [1, 5, 10, 15, 30, 60, 120, 300, 600, 900, 1800,
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import (setup, find_packages)
 
 setup(
     name="iqoptionapi",
-    version="6.8.9.1",
+    version="7.0.0",
     packages=find_packages(),
     install_requires=["pylint", "requests", "websocket-client==0.56"],
     include_package_data=True,
